5b6afb508c4ca8997f6eb740beef8c60e0b69d1e
Felipe Duarte <feduarte.dev@gmail.com>
2024-04-21 14:26:58 -0300

    requisito 10

--- tests/controllers/admin/test_admin_controller.py
+++ tests/controllers/admin/test_admin_controller.py
@@ -1,6 +1,37 @@
-# from src.models.history_model import HistoryModel
-# from src.models.user_model import UserModel
+from src.models.history_model import HistoryModel
+from src.models.user_model import UserModel
 
 
 def test_history_delete(app_test):
-    raise NotImplementedError
+    user = UserModel({"name": "Teste", "level": "admin", "token": "99999999"})
+    user.save()
+
+    history = [
+        {
+            "text-to-translate": "Hello, I like videogame",
+            "translate-from": "en",
+            "translate-to": "pt",
+        },
+        {
+            "text-to-translate": "Do you love music?",
+            "translate-from": "en",
+            "translate-to": "pt",
+        },
+    ]
+
+    for item in history:
+        item["user-id"] = user.id
+        HistoryModel(item).save()
+
+    check_history = HistoryModel.find_one(
+        {
+            "text-to-translate": "Hello, I like videogame",
+        }
+    )
+
+    check_user = UserModel.find_one({"name": "Teste"})
+
+    app_test.delete(
+        f"/admin/history/{check_history.data['_id']}",
+        headers={"Authorization": f"Bearer {check_user.data['token']}"},
+    )
